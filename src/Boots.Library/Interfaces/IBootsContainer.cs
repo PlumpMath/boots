d5e7433a853113ad4486a8e2b1d70358ae2d7f30
Timothy Moriarty <de.la.tortuga@gmail.com>
2009-08-24 22:37:53 -0400

    still fixing

---
+++ src/Boots.Library/Interfaces/IBootsContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Boots.Library.Interfaces
+{
+	public interface IBootsContainer
+	{
+		Control.ControlCollection Controls{ get; }
+
+		void AddControl(Control control);
+	}
+}
