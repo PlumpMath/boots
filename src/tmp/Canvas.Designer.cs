b521278c11c5257d7d7b93bb922133f4517c3214
Timothy Moriarty <de.la.tortuga@gmail.com>
2009-08-24 22:21:06 -0400

    try and fix my boot.library issue

---
+++ src/tmp/Canvas.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Boots.Library
+{
+	partial class Canvas
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.verticalScrollBar = new System.Windows.Forms.VScrollBar();
+			this.SuspendLayout();
+			// 
+			// verticalScrollBar
+			// 
+			this.verticalScrollBar.Dock = System.Windows.Forms.DockStyle.Right;
+			this.verticalScrollBar.Location = new System.Drawing.Point(267, 0);
+			this.verticalScrollBar.Name = "verticalScrollBar";
+			this.verticalScrollBar.Size = new System.Drawing.Size(17, 264);
+			this.verticalScrollBar.TabIndex = 0;
+			// 
+			// Canvas
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(284, 264);
+			this.Controls.Add(this.verticalScrollBar);
+			this.Name = "Canvas";
+			this.Text = "Boots";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.VScrollBar verticalScrollBar;
+	}
+}
---
+++ src/tmp/Canvas.cs
@@ -0,0 +1,28 @@
+﻿using Boots.Library.Helpers;
+using Boots.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Boots.Library
+{
+	public partial class Canvas : Form, IBootsContainer
+	{
+		public Canvas()
+		{
+			InitializeComponent();
+		}
+
+		public void AddControl(Control control)
+		{
+			control.Location = this.NextPosition();
+			System.Diagnostics.Debug.WriteLine(string.Format("Top {0}, Left {1}", control.Location.Y, control.Location.X));
+			this.Controls.Add(control);
+		}
+	}
+}
